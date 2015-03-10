1b9dd9f7093745e8b659774b80adce95ea2ea989
Filip Jonassen <filip@pc166-54.hiof.no>
2015-03-10 11:16:14 +0100

    Added url for polls/ in mysite/urls.py.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^administrator/', include(admin.site.urls)),
     url(r'^skyrim/', include(admin.site.urls)),
+    url(r'^polls/', include('polls.urls')),
 )
