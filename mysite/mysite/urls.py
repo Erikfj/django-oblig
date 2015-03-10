71f186c832bd77e5d50c56af2a4404b8def8deb8
Filip Jonassen <filip@pc166-54.hiof.no>
2015-03-10 08:33:01 +0100

    Added string "administrator" or "skyrim" to also point to localhost:8000/admin.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^administrator/', include(admin.site.urls)),
+    url(r'^skyrim/', include(admin.site.urls)),
 )
